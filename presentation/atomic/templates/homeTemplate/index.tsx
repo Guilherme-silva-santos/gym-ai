5a1191e2000b8549a9540f611e20652388484399
guilherme-silva-santos <guilhermesilvasantosjnc@gmail.com>
2025-02-21 09:26:47 -0300

    feat: create explore banner component

--- presentation/atomic/templates/homeTemplate/index.tsx
+++ presentation/atomic/templates/homeTemplate/index.tsx
@@ -1,41 +1,45 @@
 import { ScreenTemplate } from '../screenTemplate';
 
 import { useState } from 'react';
-import { Button, View } from 'react-native';
+import { Button, ScrollView, View } from 'react-native';
 import Image from '../../../../assets/images/react-logo.png';
 import { Badge } from '../../atoms/badge';
 import { Input } from '../../atoms/input';
 import { PlayerContolButton } from '../../atoms/playerContollButton';
 import { ProgressBar } from '../../atoms/progressBar';
 import { ExerciseCard } from '../../organisms/exerciseCard';
+import { ExploreBanner } from '../../organisms/ExploreBanner';
 import { PreviewVideoCard } from '../../organisms/previewVideoCard';
 
 export const HomeTemplate = () => {
   const [progress, setProgress] = useState(0);
   return (
     <ScreenTemplate title="Home">
-      <View style={{ flexDirection: 'column', padding: 20, gap: 20 }}>
-        <Input placeholder="Search" icon="search" />
-        <Badge text="500 Kcal" icon="local-fire-department" />
-        <PlayerContolButton icon="play-arrow" />
-        <PreviewVideoCard
-          image={Image}
-          kcal="500 Kcal"
-          time="30 min"
-          onpressButton={() => {}}
-        />
-        <ProgressBar progress={progress} />
-        <Button
-          title="Aumentar Progresso"
-          onPress={() => setProgress(prev => (prev >= 100 ? 0 : prev + 10))}
-        />
-        <ExerciseCard
-          image={Image}
-          title="Exercicio 1"
-          reps="10"
-          progress={progress}
-        />
-      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flexDirection: 'column', padding: 20, gap: 20 }}>
+          <Input placeholder="Search" icon="search" />
+          <Badge text="500 Kcal" icon="local-fire-department" />
+          <PlayerContolButton icon="play-arrow" />
+          <PreviewVideoCard
+            image={Image}
+            kcal="500 Kcal"
+            time="30 min"
+            onpressButton={() => {}}
+          />
+          <ProgressBar progress={progress} />
+          <Button
+            title="Aumentar Progresso"
+            onPress={() => setProgress(prev => (prev >= 100 ? 0 : prev + 10))}
+          />
+          <ExerciseCard
+            image={Image}
+            title="Exercicio 1"
+            reps="10"
+            progress={progress}
+          />
+          <ExploreBanner text="Veja mais" title="Exercicio 1" image={Image} />
+        </View>
+      </ScrollView>
     </ScreenTemplate>
   );
 };
